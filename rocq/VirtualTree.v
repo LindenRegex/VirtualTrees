e855bef4f616b7cd7d9341ddd2cbf50869bd5e15
Mathilde Peruzzo <mathilde.peruzzo@gmail.com>
2026-04-21 15:25:12 +0200

    defined state and did insert

--- rocq/VirtualTree.v
+++ rocq/VirtualTree.v
@@ -1,18 +1,61 @@
 
 Require Import CData.
 
-From Stdlib Require Import List.
-
 Module VT (Data : CDATA).
   Import Data.
 
-  Inductive VirtalTree := Admitted. (* the actal tree with root, branch, node and leaf *)
+  Inductive VirtualTree : Set := (* the actal tree with root, branch, node and leaf *)
+  | Root (child: VirtualTree)
+  | Node (data: Data.t) (child: VirtualTree)
+  | Branch (l: VirtualTree) (r: VirtualTree)
+  | Leaf (id: nat).
+    
 
-  (* Type state: counter + tree *)
+  (* Type state: counter + tree (+ param ?) *)
+  (* Definine validity for a state *)
 
-  Definition empty := Admitted.
+  Definition State := (VirtualTree * nat * Data.p)%type.
 
-  Definition insert := Admitted.
+  Definition tree (s: State) : VirtualTree :=
+    match s with
+    | (t, _, _) => t
+    end.
+  Definition cnt (s: State) : nat :=
+    match s with
+    | (_, c, _) => c
+    end.
+  Definition param (s: State) : Data.p :=
+    match s with
+    | (_, _, p) => p
+    end.
+
+  Definition is_leaf_with_id (t: VirtualTree) (id: nat) : bool :=
+    match t with
+    | Leaf i => Nat.eqb i id
+    | _ => false
+    end.
+
+  Definition empty (param: Data.p) : State :=
+    (Root(Leaf(0)), 1, param).
+
+  Fixpoint insert_in_tree (id: nat) (new: Data.t) (t: VirtualTree) : VirtualTree :=
+    match t with
+    | Root c => if is_leaf_with_id c id
+                then Root (Node new c)
+                else Root (insert_in_tree id new c)
+    | Node d c => if is_leaf_with_id c id
+                  then Node (Data.compress d new) c
+                  else Node d (insert_in_tree id new c)
+    | Branch l r => if is_leaf_with_id l id
+                    then Branch (Node new l) r
+                    else if is_leaf_with_id r id
+                         then Branch l (Node new r)
+                         else Branch (insert_in_tree id new l) (insert_in_tree id new r)
+    | Leaf i => t
+    end.
+
+  Definition insert (id: nat) (new : Data.t) (s: State) : State :=
+    (insert_in_tree id new (tree s), cnt s, param s).
 
   Definition split := Admitted.
 
