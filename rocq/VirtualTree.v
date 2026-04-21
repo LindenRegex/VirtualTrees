6ecf08bacd052586a5c916d6e2473e9faad71099
Mathilde Peruzzo <mathilde.peruzzo@gmail.com>
2026-04-21 19:39:31 +0200

    did delete

--- rocq/VirtualTree.v
+++ rocq/VirtualTree.v
@@ -5,10 +5,34 @@ Module VT (Data : CDATA).
   Import Data.
 
   Inductive VirtualTree : Set := (* the actal tree with root, branch, node and leaf *)
+  | Seed
   | Root (child: VirtualTree)
   | Node (data: Data.t) (child: VirtualTree)
   | Branch (l: VirtualTree) (r: VirtualTree)
   | Leaf (id: nat).
+
+  (* is valid : node child is not a node, any child is not a root *)
+  
+
+  Fixpoint is_valid_tree (t: VirtualTree) : bool :=
+    match t with
+    | Seed => true
+    | Root c => match c with
+                | Root _ => false
+                | _ => is_valid_tree c
+                end
+    | Node _ c => match c with
+                  | Root _ => false
+                  | Node _ _ => false
+                  | _ => is_valid_tree c
+                  end
+    | Branch l r => match l, r with
+                    | Root _, _ => false
+                    | _, Root _ => false
+                    | _, _ => (is_valid_tree l) && (is_valid_tree r)
+                    end
+    | Leaf _ => true
+    end.
     
 
   (* Type state: counter + tree (+ param ?) *)
@@ -36,10 +60,14 @@ Module VT (Data : CDATA).
     end.
 
   Definition empty (param: Data.p) : State :=
-    (Root(Leaf(0)), 1, param).
+    (Seed, 1, param).
+
+  Definition with_one_leaf (param: Data.p) : State :=
+    (Leaf 0, 1, param).
 
   Fixpoint insert_in_tree (id: nat) (new: Data.t) (t: VirtualTree) : VirtualTree :=
     match t with
+    | Seed => Seed
     | Root c => if is_leaf_with_id c id
                 then Root (Node new c)
                 else Root (insert_in_tree id new c)
@@ -59,6 +87,7 @@ Module VT (Data : CDATA).
 
   Fixpoint split_in_tree (id: nat) (new_id: nat) (t: VirtualTree) : VirtualTree :=
     match t with
+    | Seed => Seed
     | Root c => Root (split_in_tree id new_id c)
     | Node d c => Node d (split_in_tree id new_id c)
     | Branch l r => Branch (split_in_tree id new_id l) (split_in_tree id new_id r)
@@ -68,7 +97,40 @@ Module VT (Data : CDATA).
   Definition split (id: nat) (s: State) : State * nat :=
     ((split_in_tree id (cnt s) (tree s), (cnt s) + 1, param s), cnt s).
 
-  Definition delete := Admitted.
+  Fixpoint is_branch_with_id (id : nat) (t: VirtualTree) : bool :=
+    match t with
+    | Seed => false
+    | Root c => is_branch_with_id id c
+    | Node _ c => is_branch_with_id id c
+    | Branch _ _ => false
+    | Leaf i => Nat.eqb i id
+    end.
+
+  Fixpoint delete_in_tree (id : nat) (t: VirtualTree) : VirtualTree :=
+    match t with
+    | Seed => Seed
+    | Root c => Root (delete_in_tree id c)
+    | Node d c =>
+        let c' := delete_in_tree id c in
+        match c' with
+        | Seed => Seed
+        | Branch _ _ => Node d c'
+        | Node d'' c'' => Node (Data.compress d d'') c''
+        | Leaf i => Node d c'
+        | _ => t
+        end
+    | Branch l r => if (is_branch_with_id id l) (*could define it like node instead *)
+                    then r
+                    else if (is_branch_with_id id r)
+                         then l
+                         else Branch (delete_in_tree id l) (delete_in_tree id r)
+    | Leaf i => if Nat.eqb i id
+                then Seed
+                else Leaf i
+    end.
+
+  Definition delete (id: nat) (s: State) :=
+    (delete_in_tree id (tree s), cnt s, param s).
 
   Definition get_compressed_data := Admitted.
 
