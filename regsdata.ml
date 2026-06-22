881c4ed6d24a06fc270e084c003568adf022c5e0
Mathilde Peruzzo <mathilde.peruzzo@gmail.com>
2026-06-22 12:19:37 +0200

    updated regsdata and tests

--- regsdata.ml
+++ regsdata.ml
@@ -1,48 +1,200 @@
+(** * Register data *)
+(* A compressible data type for register updates *)
 
 open Array
 
 module Regsdata : sig
+  (* Data type *)
   type t = 
   | Complete of {
       a_cp: int Array.t;
       a_clk: int Array.t
     }
-  | Incomplete of (int * int *int) list
-
+  | Incomplete of {
+      size: int;
+      l: (int * int * int) list
+    }
+  (* Parameter type *)
   type p = int
 
-  val neutral_element : t
+  (* Virtual tree required functions *)
   val compress : p -> t -> t -> t
-  val to_string : t -> string
+  val copy : t -> t
+
+  (* Helpers for regs *)
   val to_arrays : int -> t -> int Array.t * int Array.t
   val get_cp_at : t -> int -> int
   val get_clk_at : t -> int -> int
+
+  (* Debugging purposes *)
+  val to_string : t -> string
 end = struct
 
+  (* Data type *)
+  (* Represents a partial register state: a state is complete once all its partial states are compressed
+  ** The incomplete form contains register updates 
+  ** The complete form contains register arrays *)
+  (* The register values can be:
+  ** -2: undefined
+  ** -1: invalid (or cleared)
+  ** A non-negative integer: a valid value *)
   type t = 
   | Complete of {
+      (* arrays of register values *)
       a_cp: int Array.t;
       a_clk: int Array.t
-    } (*arrays*)
+    }
+  | Incomplete of {
+      size: int;
+      (* list of updates *)
+      (** first int: the key *)
+      (** second int: the cp value *)
+      (** third int: the clock value *)
+      l: (int * int *int) list
+    }
 
-    (* first int: the key *)
-    (* second int: the cp value *)
-    (* third int: the clock value *)
-  | Incomplete of (int * int *int) list (*list: single or doubly ?*)
-
+  (* Parameter type *)
+  (* Here, the parameter represents the registers size *)
   type p = int
 
-  let neutral_element : t = Incomplete([])
+  (* Merges the cp arrays together and the clk arrays together *)
+  (* The new arrays overwrite the old arrays *)
+  (* Modifies and returns the a_old_cp and a_old_clk arrays *)
+  let merge_arrays (a_old_cp: int Array.t) (a_new_cp: int Array.t) 
+                   (a_old_clk: int Array.t) (a_new_clk: int Array.t) : int Array.t * int Array.t =
+    let update_oldest_cp (i: int) (new_cp: int): unit =
+      if (new_cp <> -2) then a_old_cp.(i) <- new_cp (* overwrite only if new_cp is defined *)
+      else () in
+    let update_oldest_clk (i: int) (new_clk: int): unit =
+      if (new_clk <> -2) then a_old_clk.(i) <- new_clk (* overwrite only if new_clk is defined *)
+      else () in
 
+    Array.iteri update_oldest_cp a_new_cp;
+    Array.iteri update_oldest_clk a_new_clk;
+    (a_old_cp, a_old_clk)
+
+  (* Merge a list of updates with a more recent array *)
+  (* The list only overwrites undefined array values *)
+  (* Modifies the a_cp and a_clk arrays *)
+  let rec fill_newer_array (l:(int*int*int) list) (a_cp: int Array.t) (a_clk: int Array.t) : unit =
+    match l with
+    | [] -> ()
+    | (i,cp,clk)::l' -> (* l' contains older updates *)
+        (* only update register values that are undefined *)
+        if (a_cp.(i) = -2) then a_cp.(i) <- cp;
+        if (a_clk.(i) = -2) then a_clk.(i) <- clk;
+        fill_newer_array l' a_cp a_clk
+
+  (* Merge a list of updates with an older array *)
+  (* The list overwrites the array *)
+  (* Modifies the a_cp and a_clk arrays *)
+  let rec fill_older_array (l:(int*int*int) list) (a_cp: int Array.t) (a_clk: int Array.t) : unit =
+    match l with
+    | [] -> ()
+    | (i,cp,clk)::l' -> (* l' contains older updates *)
+        (* updating the array with older updates first *)
+        fill_older_array l' a_cp a_clk;
+        (* overwriting older values if the new values are defined *)
+        if (cp <> -2) then a_cp.(i) <- cp;
+        if (clk <> -2) then a_clk.(i) <- clk        
+
+  (* Merge a list of updates with more recent arrays *)
+  (* Modifies and returns a_cp and a_clk *)
+  let compress_new_arrays_old_list (a_cp: int Array.t) (a_clk: int Array.t) (l: (int * int * int) list):
+       int Array.t * int Array.t =
+    fill_newer_array l a_cp a_clk;
+    (a_cp, a_clk)
+
+  (* Merge a list of updates with older arrays *)
+  (* Modifies and returns a_cp and a_clk *)
+  let compress_new_list_old_arrays (a_cp: int Array.t) (a_clk: int Array.t) (l: (int * int * int) list): int Array.t * int Array.t =
+    fill_older_array l a_cp a_clk;
+    (a_cp, a_clk)
+
+  (* Converts two lists of updates to registers arrays of size regs_size *)
+  let lists_to_arrays (regs_size: p) (l_old: (int * int * int) list) (l_new: (int * int * int) list) : int Array.t * int Array.t =
+    let a_cp = Array.make regs_size (-2) in
+    let a_clk = Array.make regs_size (-2) in
+    fill_newer_array l_new a_cp a_clk;
+    fill_newer_array l_old a_cp a_clk;
+    (a_cp, a_clk)
+
+  (* Compression function *)
+  (* t_old is the oldest value, and t_new is the most recent *)
+  (* regs_size is the size of the registers *)
+  let compress (regs_size: p) (t_old: t) (t_new: t): t = 
+    match t_old, t_new with
+    | Complete a_old, Complete a_new -> (* merge arrays *)
+      let (a_cp, a_clk) = merge_arrays a_old.a_cp a_new.a_cp a_old.a_clk a_new.a_clk in
+      Complete({a_cp=a_cp; a_clk=a_clk})
+    | Complete a_old, Incomplete l_new -> (* l_new overwrites a_old *)
+      let (a_cp, a_clk) = compress_new_list_old_arrays a_old.a_cp a_old.a_clk l_new.l in
+      Complete({a_cp=a_cp; a_clk=a_clk})
+    | Incomplete l_old, Complete a_new -> (* incorporate l_old into a_new where a_new is -2 ("undefined") *)
+      let (a_cp, a_clk) = compress_new_arrays_old_list a_new.a_cp a_new.a_clk l_old.l in
+      Complete({a_cp=a_cp; a_clk=a_clk}) 
+    | Incomplete l_old, Incomplete l_new ->
+      let size = l_new.size + l_old.size in
+      (* if result has size larger that the registers size, convert to complete form *)
+      if size >= regs_size then (
+        let (a_cp, a_clk) = lists_to_arrays regs_size l_old.l l_new.l in
+        Complete({a_cp=a_cp; a_clk=a_clk})
+      ) else (
+        (* concatenate, most recent updates go first *)
+        Incomplete({size=size; l=(l_new.l @ l_old.l)})
+      )
+
+  (* Copies a value of type t so that the copy can be modified without changes to the original *)
+  let copy (t: t) : t =
+    match t with
+    | Complete arrays -> (* arrays are mutable *)
+      Complete({a_cp = Array.copy arrays.a_cp; a_clk = Array.copy arrays.a_clk})
+    | Incomplete _ -> t (* lists are immutable *)
+
+  (* Converts a value of type t to registers in their "array" form *)
+  let to_arrays (size: int) (v: t) : int Array.t * int Array.t =
+    match v with 
+    | Complete arrays -> (arrays.a_cp, arrays.a_clk)
+    | Incomplete l -> compress_new_list_old_arrays (Array.make size (-2)) (Array.make size (-2)) l.l
+
+  (* Getter for most recent cp value at index k in t *)
+  (* Returns -2 if that value is undefined *)
+  let get_cp_at (t: t) (k: int) : int =
+    match t with
+    | Incomplete l -> (* check if l contains an update for k *)
+      let rec get_cp_rec (l:(int*int*int) list) : int =
+        match l with
+        | [] -> -2 (* l has no updates for index k, return "undefined" *)
+        | (kl,cp,clk)::l' ->
+           if (kl = k) then cp
+           else get_cp_rec l' in
+      get_cp_rec l.l
+    | Complete a -> a.a_cp.(k)
+
+  (* Getter for most recent clk value at index k in t *)
+  (* Returns -2 if that value is undefined *)
+  let get_clk_at (t: t) (k: int) : int =
+    match t with
+    | Incomplete l -> (* check if l contains an update for k *)
+      let rec get_clk_rec (l:(int*int*int) list) : int =
+        match l with
+        | [] -> -2 (* l has no updates for index k, return "undefined" *)
+        | (kl,cp,clk)::l' ->
+           if (kl = k) then clk
+           else get_clk_rec l' in
+      get_clk_rec l.l
+    | Complete a -> a.a_clk.(k)
+
+  (* Converts a value of type t to a string *)
   let to_string (v: t) : string =
     match v with 
     | Complete arrays ->
       let s = ref "CP: " in
-      for c = 0 to (Array.length arrays.a_cp)-1 do
+      for c = 0 to (Array.length arrays.a_cp) - 1 do
         s := !s ^ string_of_int c ^ ": " ^ string_of_int (arrays.a_cp.(c)) ^ " | "
       done;
       s := !s ^ " CLK: ";
-      for c = 0 to (Array.length arrays.a_clk)-1 do
+      for c = 0 to (Array.length arrays.a_clk) - 1 do
         s := !s ^ string_of_int c ^ ": " ^ string_of_int (arrays.a_clk.(c)) ^ " | "
       done;
       !s
@@ -53,93 +205,6 @@ end = struct
       | (i,cp,clk)::l' ->
          "(" ^ string_of_int i ^ "," ^ string_of_int cp ^ "," ^ string_of_int clk ^ ")::" ^ to_string_rec l'
       in
-      to_string_rec l
-
-  (* a2 is more recent than a1 *)
-  let merge_arrays (a1_cp: int Array.t) (a2_cp: int Array.t) (a1_clk: int Array.t) (a2_clk: int Array.t) : int Array.t * int Array.t =
-    (* update a1 *)
-    let update_oldest_cp (i: int) (new_cp: int): unit =
-      if (new_cp <> -1) then a1_cp.(i) <- new_cp
-      else () in
-    let update_oldest_clk (i: int) (new_clk: int): unit =
-      if (new_clk <> -1) then a1_clk.(i) <- new_clk
-      else () in
-
-    Array.iteri update_oldest_cp a2_cp;
-    Array.iteri update_oldest_clk a2_clk;
-    (a1_cp, a1_clk)
-
-  (* array is more recent than list: list cannot overwrite array *)
-  let compress_arrays_and_list (a_cp: int Array.t) (a_clk: int Array.t) (l: (int * int * int) list) : int Array.t * int Array.t =
-    let rec fill_array (l:(int*int*int) list) : unit =
-        match l with
-        | [] -> ()
-        | (i,cp,clk)::l' ->
-            (* only setting reg values that haven't been set yet *)
-            if (a_cp.(i) = -1) then a_cp.(i) <- cp;
-            if (a_clk.(i) = -1) then a_clk.(i) <- clk;
-            fill_array l' in
-    fill_array l;
-    (a_cp, a_clk)
-
-  (* list is more recent than array, list overwrites array *)
-  (* Problem: how to know if we overwrite array or list ? *)
-  (* Solution 1: use doubly linked list like in Théo's paper *)
-  (* Solution 2: convert l to array and merge arrays *)
-  let compress_list_and_arrays (a_cp: int Array.t) (a_clk: int Array.t) (l: (int * int * int) list): int Array.t * int Array.t =
-    let size = Array.length a_cp in
-    let (l_cp, l_clk) = compress_arrays_and_list (Array.make size (-1)) (Array.make size (-1)) l in
-    merge_arrays a_cp l_cp a_clk l_clk
-
-  (* t1 is "oldest", t2 is "most recent" *)
-  let compress (regs_size: p) (t1: t) (t2: t): t = 
-    match t1, t2 with
-    | Complete a1, Complete a2 -> (* merge arrays *)
-      let (a_cp, a_clk) = merge_arrays a1.a_cp a2.a_cp a1.a_clk a2.a_clk in
-      Complete({a_cp=a_cp; a_clk=a_clk})
-    | Complete a1, Incomplete l2 -> (* incorporate l2 in a1 *)
-      let (a_cp, a_clk) = compress_list_and_arrays a1.a_cp a1.a_clk l2 in
-      Complete({a_cp=a_cp; a_clk=a_clk}) 
-    | Incomplete l1, Complete a2 -> (* incorporate l1 into a2*)
-      let (a_cp, a_clk) = compress_arrays_and_list a2.a_cp a2.a_clk l1 in
-      Complete({a_cp=a_cp; a_clk=a_clk}) 
-    | Incomplete l1, Incomplete l2 ->
-      (* concatenate *)
-      let l = l2 @ l1 in 
-      (* if result has size larger that |r|, convert to complete form *)
-      if List.length l >= regs_size then (
-        let (a_cp, a_clk) = compress_arrays_and_list (Array.make regs_size (-1)) (Array.make regs_size (-1)) l in
-        Complete({a_cp=a_cp; a_clk=a_clk})
-      ) else 
-        Incomplete(l)
-
-  let to_arrays (size: int) (v: t) : int Array.t * int Array.t =
-    match v with 
-    | Complete arrays -> (arrays.a_cp, arrays.a_clk)
-    | Incomplete l -> compress_arrays_and_list (Array.make size (-1)) (Array.make size (-1)) l
-
-  let get_cp_at (t: t) (k: int) : int =
-    match t with
-    | Incomplete l -> (* check if l contains an update for k *)
-      let rec get_rec (l:(int*int*int) list) : int =
-        match l with
-        | [] -> -1
-        | (kl,cp,clk)::l' ->
-           if (kl = k) then cp
-           else get_rec l' in
-      get_rec l
-    | Complete a -> a.a_cp.(k)
-
-  let get_clk_at (t: t) (k: int) : int =
-    match t with
-    | Incomplete l -> (* check if l contains an update for k *)
-      let rec get_rec (l:(int*int*int) list) : int =
-        match l with
-        | [] -> -1
-        | (kl,cp,clk)::l' ->
-           if (kl = k) then clk
-           else get_rec l' in
-      get_rec l
-    | Complete a -> a.a_clk.(k)
+      to_string_rec l.l
 
 end
--- regsdatatests.ml
+++ regsdatatests.ml
@@ -5,9 +5,6 @@ module Tests: sig
   val tests : unit -> unit
 end = struct
 
-  let neutral_test () = 
-    assert(Regsdata.neutral_element = Incomplete([]))
-
   let two_arrays_test0 () =
     let a_old = Regsdata.Complete({a_cp=[|1; 2; 3; 4|]; a_clk=[|5; 6; 7; 8|]}) in
     let a_new = Regsdata.Complete({a_cp=[|9; 10; 11; 12|]; a_clk=[|13; 14; 15; 16|]}) in
@@ -16,103 +13,145 @@ end = struct
     assert(a = a_old) (* because old is mutated *)
 
   let two_arrays_test1 () =
-    let a_old = Regsdata.Complete({a_cp=[|1; 2; -1; 4|]; a_clk=[|-1; 6; 7; -1|]}) in
+    let a_old = Regsdata.Complete({a_cp=[|1; -2; -1; 4|]; a_clk=[|-1; 6; -2; -1|]}) in
     let a_new = Regsdata.Complete({a_cp=[|9; 10; 11; -1|]; a_clk=[|-1; 14; -1; 16|]}) in
     let a = Regsdata.compress 4 a_old a_new in
-    assert(a = Regsdata.Complete({a_cp=[|9; 10; 11; 4|]; a_clk=[|-1; 14; 7; 16|]}));
+    assert(a = Regsdata.Complete({a_cp=[|9; 10; 11; -1|]; a_clk=[|-1; 14; -1; 16|]}));
+    assert(a = a_old) (* because old is mutated *)
+
+  let two_arrays_test2 () =
+    let a_old = Regsdata.Complete({a_cp=[|1; 2; -1; 4|]; a_clk=[|-2; 6; 7; -2|]}) in
+    let a_new = Regsdata.Complete({a_cp=[|9; 10; 11; -2|]; a_clk=[|-2; -2; -1; 16|]}) in
+    let a = Regsdata.compress 4 a_old a_new in
+    assert(a = Regsdata.Complete({a_cp=[|9; 10; 11; 4|]; a_clk=[|-2; 6; -1; 16|]}));
     assert(a = a_old) (* because old is mutated *)
 
   let two_lists_test0 () =
-    let l_old = Regsdata.Incomplete([(1, 1, 1); (2, 2, 2)]) in
-    let l_new = Regsdata.Incomplete([(3, 3, 3); (4, 4, 4)]) in
+    let l_old = Regsdata.Incomplete({size=2; l=[(1, 1, 1); (2, 2, 2)]}) in
+    let l_new = Regsdata.Incomplete({size=2; l=[(3, 3, 3); (4, 4, 4)]}) in
     let l = Regsdata.compress 5 l_old l_new in
-    assert (l = Regsdata.Incomplete([(3, 3, 3); (4, 4, 4); (1, 1, 1); (2, 2, 2)]))
+    assert (l = Regsdata.Incomplete({size=4; l=[(3, 3, 3); (4, 4, 4); (1, 1, 1); (2, 2, 2)]}))
 
   let two_lists_test1 () =
-    let l_old = Regsdata.Incomplete([(1, 1, 1); (2, 2, 2)]) in
-    let l_new = Regsdata.Incomplete([(3, 3, 3); (4, 4, 4); (0, 0, 0)]) in
+    let l_old = Regsdata.Incomplete({size=2; l=[(1, 1, 1); (2, 2, 2)]}) in
+    let l_new = Regsdata.Incomplete({size=3; l=[(3, 3, 3); (4, 4, 4); (0, 0, 0)]}) in
     let l = Regsdata.compress 5 l_old l_new in
     assert (l = Regsdata.Complete({a_cp=[|0; 1; 2; 3; 4|]; a_clk=[|0; 1; 2; 3; 4|]}))
 
   let two_lists_test2 () =
-    let l_old = Regsdata.Incomplete([(1, 1, 1); (2, 2, 2)]) in
-    let l_new = Regsdata.Incomplete([(3, 3, 3); (4, 4, 4); (1, 0, 0)]) in
+    let l_old = Regsdata.Incomplete({size=2; l=[(1, -1, -1); (2, 2, 2)]}) in
+    let l_new = Regsdata.Incomplete({size=3; l=[(3, 3, 3); (4, 4, 4); (1, 0, 0)]}) in
     let l = Regsdata.compress 5 l_old l_new in
-    assert (l = Regsdata.Complete({a_cp=[|-1; 0; 2; 3; 4|]; a_clk=[|-1; 0; 2; 3; 4|]}))
+    assert (l = Regsdata.Complete({a_cp=[|-2; 0; 2; 3; 4|]; a_clk=[|-2; 0; 2; 3; 4|]}))
 
   let two_lists_test3 () =
-    let l_old = Regsdata.Incomplete([(1, 1, 1); (2, 5, 5); (2, 2, 2); (1, 7, 7)]) in
-    let l_new = Regsdata.Incomplete([(3, 6, 6); (1, 0, 0); (3, 3, 3)]) in
+    let l_old = Regsdata.Incomplete({size=4; l=[(1, 1, 1); (2, 5, 5); (2, 2, 2); (1, 7, 7)]}) in
+    let l_new = Regsdata.Incomplete({size=3; l=[(3, -1, -1); (1, 0, 0); (3, 3, 3)]}) in
     let l = Regsdata.compress 5 l_old l_new in
-    assert (l = Regsdata.Complete({a_cp=[|-1; 0; 5; 6; -1|]; a_clk=[|-1; 0; 5; 6; -1|]}))
+    assert (l = Regsdata.Complete({a_cp=[|-2; 0; 5; -1; -2|]; a_clk=[|-2; 0; 5; -1; -2|]}))
+
+  let two_lists_test4 () =
+    let l_old = Regsdata.Incomplete({size=4; l=[(1, 1, 1); (2, 5, 5); (2, 2, 2); (1, 7, 7)]}) in
+    let l_new = Regsdata.Incomplete({size=3; l=[(3, -2, -2); (1, 0, 0); (3, 3, 3)]}) in
+    let l = Regsdata.compress 5 l_old l_new in
+    assert (l = Regsdata.Complete({a_cp=[|-2; 0; 5; 3; -2|]; a_clk=[|-2; 0; 5; 3; -2|]}))
 
   let list_array_test0 () = 
-    let a_old = Regsdata.Complete({a_cp=[|1; 2; 3; 4; 5|]; a_clk=[|6; 7; 8; 9; 10|]}) in
-    let l_new = Regsdata.Incomplete([(0, 0, 0); (0, 11, 11); (3, 12, 12)]) in
+    let a_old = Regsdata.Complete({a_cp=[|-2; 2; 3; 4; 5|]; a_clk=[|6; 7; 8; 9; 10|]}) in
+    let l_new = Regsdata.Incomplete({size=3; l=[(0, 0, 0); (0, 11, 11); (3, 12, 12)]}) in
     let l = Regsdata.compress 5 a_old l_new in
     assert (l = Regsdata.Complete({a_cp=[|0; 2; 3; 12; 5|]; a_clk=[|0; 7; 8; 12; 10|]}))
   
   let list_array_test1 () = 
     let a_old = Regsdata.Complete({a_cp=[|1; 2; 3; -1; -1|]; a_clk=[|6; 7; 8; -1; -1|]}) in
-    let l_new = Regsdata.Incomplete([(0, -1, 0); (3, 11, -1); (3, 12, 12)]) in
+    let l_new = Regsdata.Incomplete({size=3; l=[(0, -1, 0); (3, 11, -1); (3, 12, 12)]}) in
+    let l = Regsdata.compress 5 a_old l_new in
+    assert (l = Regsdata.Complete({a_cp=[|-1; 2; 3; 11; -1|]; a_clk=[|0; 7; 8; -1; -1|]}))
+
+  let list_array_test2 () = 
+    let a_old = Regsdata.Complete({a_cp=[|1; 2; 3; -1; -1|]; a_clk=[|6; 7; 8; -1; -1|]}) in
+    let l_new = Regsdata.Incomplete({size=3; l=[(0, -2, 0); (3, 11, -2); (3, 12, 12)]}) in
     let l = Regsdata.compress 5 a_old l_new in
     assert (l = Regsdata.Complete({a_cp=[|1; 2; 3; 11; -1|]; a_clk=[|0; 7; 8; 12; -1|]}))
 
   let array_list_test0 () = 
-    let l_old = Regsdata.Incomplete([(0, 0, 0); (3, 11, 11); (3, 12, 12)]) in
+    let l_old = Regsdata.Incomplete({size=3; l=[(0, 0, 0); (3, 11, 11); (3, 12, 12)]}) in
     let a_new = Regsdata.Complete({a_cp=[|1; 2; 3; 4; 5|]; a_clk=[|6; 7; 8; 9; 10|]}) in
     let l = Regsdata.compress 5 l_old a_new in
     assert (l = Regsdata.Complete({a_cp=[|1; 2; 3; 4; 5|]; a_clk=[|6; 7; 8; 9; 10|]}))
 
   let array_list_test1 () = 
-    let l_old = Regsdata.Incomplete([(0, 0, 0); (3, 11, 11); (3, 12, 12)]) in
+    let l_old = Regsdata.Incomplete({size=3; l=[(0, 0, 0); (3, 11, 11); (3, 12, 12)]}) in
     let a_new = Regsdata.Complete({a_cp=[|1; 2; 3; 4; -1|]; a_clk=[|6; 7; 8; 9; -1|]}) in
     let l = Regsdata.compress 5 l_old a_new in
     assert (l = Regsdata.Complete({a_cp=[|1; 2; 3; 4; -1|]; a_clk=[|6; 7; 8; 9; -1|]}))
 
   let array_list_test2 () = 
-    let l_old = Regsdata.Incomplete([(0, 0, 0); (3, 11, 11); (3, 12, 12)]) in
+    let l_old = Regsdata.Incomplete({size=3; l=[(0, 0, 0); (3, 11, 11); (3, 12, 12)]}) in
     let a_new = Regsdata.Complete({a_cp=[|1; 2; 3; -1; -1|]; a_clk=[|6; 7; 8; -1; -1|]}) in
     let l = Regsdata.compress 5 l_old a_new in
-    assert (l = Regsdata.Complete({a_cp=[|1; 2; 3; 11; -1|]; a_clk=[|6; 7; 8; 11; -1|]}))
+    assert (l = Regsdata.Complete({a_cp=[|1; 2; 3; -1; -1|]; a_clk=[|6; 7; 8; -1; -1|]}))
 
   let array_list_test3 () = 
-    let l_old = Regsdata.Incomplete([(0, -1, 0); (3, 11, 11); (3, 12, 12); (1, 13, 13)]) in
+    let l_old = Regsdata.Incomplete({size=4; l=[(0, -1, 0); (3, -2, -2); (3, 12, 12); (1, 13, 13)]}) in
     let a_new = Regsdata.Complete({a_cp=[|1; -1; 3; -1; -1|]; a_clk=[|6; 7; 8; 9; -1|]}) in
     let l = Regsdata.compress 5 l_old a_new in
-    assert (l = Regsdata.Complete({a_cp=[|1; 13; 3; 11; -1|]; a_clk=[|6; 7; 8; 9; -1|]}))
+    assert (l = Regsdata.Complete({a_cp=[|1; -1; 3; -1; -1|]; a_clk=[|6; 7; 8; 9; -1|]}))
+
+  let array_list_test4 () = 
+    let l_old = Regsdata.Incomplete({size=3; l=[(0, 0, 0); (3, 11, 11); (3, 12, 12)]}) in
+    let a_new = Regsdata.Complete({a_cp=[|1; 2; 3; 4; -2|]; a_clk=[|6; 7; 8; 9; -2|]}) in
+    let l = Regsdata.compress 5 l_old a_new in
+    assert (l = Regsdata.Complete({a_cp=[|1; 2; 3; 4; -2|]; a_clk=[|6; 7; 8; 9; -2|]}))
+
+  let array_list_test5 () = 
+    let l_old = Regsdata.Incomplete({size=3; l=[(0, 0, 0); (3, 11, 11); (3, 12, 12)]}) in
+    let a_new = Regsdata.Complete({a_cp=[|1; 2; 3; -2; -2|]; a_clk=[|6; 7; 8; -2; -2|]}) in
+    let l = Regsdata.compress 5 l_old a_new in
+    assert (l = Regsdata.Complete({a_cp=[|1; 2; 3; 11; -2|]; a_clk=[|6; 7; 8; 11; -2|]}))
+
+  let array_list_test6 () = 
+    let l_old = Regsdata.Incomplete({size=4; l=[(0, -2, 0); (3, 11, 11); (3, 12, 12); (1, 13, 13)]}) in
+    let a_new = Regsdata.Complete({a_cp=[|1; -2; 3; -2; -2|]; a_clk=[|6; 7; 8; 9; -2|]}) in
+    let l = Regsdata.compress 5 l_old a_new in
+    assert (l = Regsdata.Complete({a_cp=[|1; 13; 3; 11; -2|]; a_clk=[|6; 7; 8; 9; -2|]}))
 
   let array_get_test () = 
-    let a = Regsdata.Complete({a_cp=[|1; 5; 3; 9; -1|]; a_clk=[|-1; 7; 8; 9; -1|]}) in
+    let a = Regsdata.Complete({a_cp=[|1; 5; 3; 9; -1|]; a_clk=[|-2; 7; 8; 9; -1|]}) in
     assert ((Regsdata.get_cp_at a 1) = 5);
     assert ((Regsdata.get_cp_at a 4) = -1);
     assert ((Regsdata.get_clk_at a 2) = 8);
-    assert ((Regsdata.get_clk_at a 0) = -1)
+    assert ((Regsdata.get_clk_at a 0) = -2)
 
   let list_get_test () = 
-    let l = Regsdata.Incomplete([(0, 0, 1); (3, 10, 11); (3, 12, 13)]) in
+    let l = Regsdata.Incomplete({size=3; l=[(0, 0, 1); (3, -1, -2); (3, 12, 13)]}) in
     assert ((Regsdata.get_cp_at l 0) = 0);
-    assert ((Regsdata.get_cp_at l 3) = 10);
-    assert ((Regsdata.get_cp_at l 4) = -1);
+    assert ((Regsdata.get_cp_at l 3) = -1);
+    assert ((Regsdata.get_cp_at l 4) = -2);
     assert ((Regsdata.get_clk_at l 0) = 1);
-    assert ((Regsdata.get_clk_at l 3) = 11);
-    assert ((Regsdata.get_clk_at l 1) = -1)
+    assert ((Regsdata.get_clk_at l 3) = -2);
+    assert ((Regsdata.get_clk_at l 1) = -2)
 
   let tests () = 
     Printf.printf "\027[32mTests: \027[0m\n\n";
-    neutral_test();
     two_arrays_test0();
     two_arrays_test1();
+    two_arrays_test2();
     two_lists_test0();
     two_lists_test1();
     two_lists_test2();
     two_lists_test3();
+    two_lists_test4();
     list_array_test0();
     list_array_test1();
+    list_array_test2();
     array_list_test0();
     array_list_test1();
     array_list_test2();
     array_list_test3();
+    array_list_test4();
+    array_list_test5();
+    array_list_test6();
     array_get_test();
     list_get_test();
     Printf.printf "\027[32mTests passed\027[0m\n"
